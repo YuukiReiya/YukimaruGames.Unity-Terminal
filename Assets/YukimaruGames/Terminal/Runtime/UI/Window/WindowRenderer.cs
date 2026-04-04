6ece85a4384de3ac316b327e1c538c24ebef3de8
YuukiReiya <yuukibamba@gmail.com>
2026-04-04 11:57:15 +0900

    fix: コンストラクタでカラー情報を上書きしていた問題の修正

--- Assets/YukimaruGames/Terminal/Runtime/UI/Window/WindowRenderer.cs
+++ Assets/YukimaruGames/Terminal/Runtime/UI/Window/WindowRenderer.cs
@@ -6,14 +6,10 @@ namespace YukimaruGames.Terminal.UI.Window
 {
     public sealed class WindowRenderer : IWindowRenderer
     {
-        private readonly IPixelTextureRepository _textureRepository;
         private readonly Lazy<GUIStyle> _styleLazy;
 
         public WindowRenderer(IPixelTextureRepository pixelTextureRepository)
         {
-            _textureRepository = pixelTextureRepository;
-            _textureRepository.SetColor(Constants.ColorPalette.Window, Color.black);
-
             _styleLazy = new Lazy<GUIStyle>(new GUIStyle()
             {
                 normal = new GUIStyleState
