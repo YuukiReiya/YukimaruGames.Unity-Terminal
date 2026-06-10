7049bd31a6a1d555b943c8fa57228cc791a4402b
YuukiReiya <yuukibamba@gmail.com>
2026-06-10 23:23:21 +0900

    add: ベンチマークのReport出力プロジェクト追加

---
+++ Benchmarks/YukimaruGames.Terminal.Benchmarks/Program.cs
@@ -0,0 +1,16 @@
+﻿using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Running;
+
+namespace YukimaruGames.Terminal.Benchmarks
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            var config = DefaultConfig.Instance.WithOption(ConfigOptions.DisableOptimizationsValidator, true);
+            var outputPath = Path.Combine(Directory.GetCurrentDirectory(), "../../../../BenchmarkReports/");
+            config.WithArtifactsPath(outputPath);
+            var summary = BenchmarkRunner.Run<TerminalColorBenchmarks>(config);
+        }
+    }
+}
